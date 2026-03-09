0628645e02f0333053c8c78bc50d5b170d8c461b
Natalia Vieira <nataliabortolivieira@gmail.com>
2026-03-09 15:31:43 -0300

    feat: implementação requisito 2 - geração de número aleatório

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -3,13 +3,19 @@
 // 2. Nosso jogo deve ferar um número secreto aleatório
 // 3. Nosso jogo deve validar a tentativa do jogador e exibir uma mensagem
 
+using System;
+using System.Security.Cryptography; //Biblioteca padrão de criptografia
+
 Console.WriteLine("--------------------------");
 Console.WriteLine("Jogo de Adivinhação");
 Console.WriteLine("--------------------------");
+
+int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
 
 Console.Write("Digite um número entre 1 e 20: ");
 string? chute = Console.ReadLine();
 
 Console.WriteLine("O valor digitado foi: " + chute);
-Console.ReadLine();
+Console.WriteLine("O valor aleatório era: " + numeroAleatorio);
 
+Console.ReadLine();
--- JogoDeAdivinhacao.ConsoleApp/obj/Debug/net10.0/JogoDeAdivinhacao.ConsoleApp.AssemblyInfo.cs
+++ JogoDeAdivinhacao.ConsoleApp/obj/Debug/net10.0/JogoDeAdivinhacao.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogoDeAdivinhacao.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd265ae315ec0c780abb59e1efaabca9b8776d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ef81280a86f2a246615ede0562d8cb41189565d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogoDeAdivinhacao.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogoDeAdivinhacao.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
